63df1a3ca1605803a210178b2e4ebbdf4971ec22
Thomas Simon <thomas.simon@btopenworld.com>
2019-11-01 01:59:39 +0000

    Added project

---
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    protected Rigidbody playerRigidbody;
+
+    protected Vector3 moveInput;
+    protected Vector3 moveVelocity;
+    
+    void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody.velocity = moveVelocity;
+    }
+}
---
+++ New Unity Project/Assets/PlayerRotation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotation : MonoBehaviour
+{
+    [SerializeField] protected float rotSpeed;
+    [SerializeField] protected float rotationDeadZone;
+    [SerializeField] protected float rotationOffset;
+
+    void Update()
+    {
+        RotateToMouse();
+    }
+
+    void RotateToMouse()
+    {
+        Vector3 positionOnScreen = Camera.main.WorldToViewportPoint(transform.position);
+        Vector3 mouseOnScreen = (Vector3)Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+        float angle = AngleBetweenTwoPoints(positionOnScreen, mouseOnScreen);
+
+        Quaternion newQuat = Quaternion.Euler(new Vector3 (0f, -angle - rotationOffset, 0f));
+        transform.rotation = Quaternion.Lerp(transform.rotation, newQuat, Time.time * rotSpeed);
+    }
+
+    float AngleBetweenTwoPoints(Vector3 a, Vector3 b)
+    {
+        return Mathf.Atan2(a.y - b.y, a.x - b.x) * Mathf.Rad2Deg;
+    }
+}
